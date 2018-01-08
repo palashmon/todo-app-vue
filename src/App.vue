f50b0896d226da3a266d878a6214880b56d6bc84
Palash Mondal <palashm@live.in>
2018-01-08 22:52:33 +0530

    Adding & rendering component data

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <!-- Render the TodoList component -->
     <!-- TodoList becomes -->
-    <todo-list></todo-list>
+    <todo-list :todos="todos"></todo-list>
   </div>
 </template>
 
@@ -14,6 +14,33 @@ export default {
     components: {
         // Add a reference to the TodoList component in the components property
         TodoList
+    },
+    // data function avails data to the template
+    data() {
+        return {
+            todos: [
+                {
+                    title: 'Todo A',
+                    project: 'Project A',
+                    done: false
+                },
+                {
+                    title: 'Todo B',
+                    project: 'Project B',
+                    done: true
+                },
+                {
+                    title: 'Todo C',
+                    project: 'Project C',
+                    done: false
+                },
+                {
+                    title: 'Todo D',
+                    project: 'Project D',
+                    done: false
+                }
+            ]
+        };
     }
 };
 </script>
@@ -24,6 +51,6 @@ export default {
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
-    margin-top: 60px;
+    padding-top: 40px;
 }
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,15 +1,36 @@
 <template>
   <div>
-    <ul>
-        <li> Todo A </li>
-        <li> Todo B </li>
-        <li> Todo C </li>
-    </ul>
+    <!-- JavaScript expressions in Vue are enclosed in double curly brackets. -->
+    <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
+    <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
+    <div class='ui centered card' v-for="(todo, index) in todos" :key="index">
+      <div class='content'>
+        <div class='header'>
+          {{ todo.title }}
+        </div>
+        <div class='meta'>
+          {{ todo.project }}
+        </div>
+        <div class='extra content'>
+          <span class='right floated edit icon'>
+            <i class='edit icon'></i>
+          </span>
+        </div>
+      </div>
+      <div class='ui bottom attached green basic button' v-show="todo.done">
+        Completed
+      </div>
+      <div class='ui bottom attached red basic button' v-show="!todo.done">
+        Complete
+      </div>
+    </div>
   </div>
 </template>
 
 <script type = "text/javascript" >
-export default {};
+export default {
+    props: ['todos']
+};
 </script>
 <style>
 
