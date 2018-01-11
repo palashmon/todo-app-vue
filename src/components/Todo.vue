f74f36565f4f767a6d3c7db2b778bb82ef97f6c4
Palash Mondal <palashm@live.in>
2018-01-11 21:11:28 +0530

    Editing a Todo

---
+++ src/components/Todo.vue
@@ -0,0 +1,60 @@
+<template>
+    <div class="ui centered card">
+        <div class="content">
+            <div class="header">
+                {{ todo.title }}
+            </div>
+            <div class="meta">
+                {{ todo.project }}
+            </div>
+            <div class="extra content">
+                <span class="right floated edit icon" @click="showForm">
+                    <i class="edit icon"></i>
+                </span>
+            </div>
+        </div>
+        <!-- form is visible when we are in editing mode -->
+        <div class="content" v-show="isEditing">
+            <div class='ui form'>
+                <div class='field'>
+                <label>Title</label>
+                <input type='text' v-model="todo.title" >
+                </div>
+                <div class='field'>
+                <label>Project</label>
+                <input type='text' v-model="todo.project" >
+                </div>
+                <div class='ui two button attached buttons'>
+                <button class='ui basic blue button' v-on:click="hideForm">
+                    Close X
+                </button>
+                </div>
+            </div>
+        </div>
+        <div class="ui bottom attached green basic button" v-show="todo.done">
+            Completed
+        </div>
+        <div class="ui bottom attached red basic button" v-show="!todo.done">
+            Complete
+        </div>
+    </div>
+</template>
+
+<script type="text/javascript">
+export default {
+    props: ['todo'],
+    data() {
+        return {
+            isEditing: false
+        };
+    },
+    methods: {
+        showForm() {
+            this.isEditing = true;
+        },
+        hideForm() {
+            this.isEditing = false;
+        }
+    }
+};
+</script>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -3,33 +3,17 @@
     <!-- JavaScript expressions in Vue are enclosed in double curly brackets. -->
     <p>Completed Tasks: {{todos.filter(todo => {return todo.done === true}).length}}</p>
     <p>Pending Tasks: {{todos.filter(todo => {return todo.done === false}).length}}</p>
-    <div class='ui centered card' v-for="(todo, index) in todos" :key="index">
-      <div class='content'>
-        <div class='header'>
-          {{ todo.title }}
-        </div>
-        <div class='meta'>
-          {{ todo.project }}
-        </div>
-        <div class='extra content'>
-          <span class='right floated edit icon'>
-            <i class='edit icon'></i>
-          </span>
-        </div>
-      </div>
-      <div class='ui bottom attached green basic button' v-show="todo.done">
-        Completed
-      </div>
-      <div class='ui bottom attached red basic button' v-show="!todo.done">
-        Complete
-      </div>
-    </div>
+    <todo v-for="(todo, index) in todos" :todo="todo" :key="index"></todo>
   </div>
 </template>
 
 <script type = "text/javascript" >
+import Todo from './Todo';
 export default {
-    props: ['todos']
+    props: ['todos'],
+    components: {
+        Todo
+    }
 };
 </script>
 <style>
